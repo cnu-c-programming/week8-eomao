8d7c1af00bfc2b636e02544e6c1d79b28a0dbe76
eomao <156084917+eomao@users.noreply.github.com>
2026-04-24 11:54:43 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     char arr[] = "hello";
     char *p = "hello";
 
     arr[0] = 'H';
     printf("%s\n", arr);
-    p[0] = 'H';
-    printf("%s\n", p);
 
     return 0;
 }
