04da82ca77cbf277cd68f6280f7042b43c6b107d
eomao <156084917+eomao@users.noreply.github.com>
2026-04-24 11:56:36 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
     char *p = "hello";
-    char arr[] = 'hello';
+    char arr[] = "hello";
 
     printf("%zu %zu\n", sizeof(p), strlen(p));
     printf("%zu %zu\n", sizeof(arr), strlen(arr));
